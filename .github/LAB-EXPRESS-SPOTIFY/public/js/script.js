225312a55d7d577752f8e0035c6f51d19c855add
unknown <jimmyjamesjj>
2021-02-09 08:38:56 +0100

    was unable to deburg error on iteration3 which messed up everything

---
+++ .github/LAB-EXPRESS-SPOTIFY/public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("LAB-EXPRESS-SPOTIFY JS imported successfully!");
+  },
+  false
+);
--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
-
+const SpotifyWebApi = require('spotify-web-api-node');
 const app = express();
 
 app.set('view engine', 'hbs');
@@ -12,7 +12,45 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
 
+app.get("/", (req, res, next) => {
+        // directly show an hbs page to the user
+        res.render('home.hbs') 
+  });
+  app.get('/artists', (req, res) => {
+    let artist = req.query.artist;
+  
+    spotifyApi.searchArtists(req.query.artist)
+      .then( (data) =>{
+        res.render('artist', {
+          artists: data.body.artists.items
+        });
+      }, 
+      (err)=> {
+        console.log('Something went wrong when loading', err);
+      });
+  });
+
+  app.get('/albums/:artistId', (req, res, next) => {
+    spotifyApi.getArtistAlbums(req.params.artistId)
+      .then( (data) =>{
+        res.render('albums.hbs', {
+          albums: data.body.items
+        });
+      },  (err) =>{
+        console.log('Something went wrong while loading page', err);
+      });
+  });
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
